5b1460fa757a95ebaca2942d8703c02a43b28002
Taylor-Mendez <tlmendez0302@my.msutexas.edu>
2021-09-09 14:05:49 -0500

    jolly jumpers

---
+++ Assignments/P10038/main.cpp
@@ -0,0 +1,88 @@
+/**
+* Taylor Mendez
+* 4883-Programming Techniques
+* 09/09/2021
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <iomanip>
+
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+
+    ifstream cin("input4.txt");
+
+    int A, B;                   // to read in first value in line and the rest of the values
+    vector <int> V;             // to put data in
+    bool jolly = true;          
+    int diff = 0;
+
+    while (cin >> A){
+
+        if (A == 1){
+            cout << "Jolly" << endl;
+            cin >> A;
+        }
+
+        else {
+            vector <int> v((A-1), 0);      // to check
+            int hits = 0;                  // to check     
+
+
+            for (int i = 0; i < A; i++){
+                cin >> B;
+                V.push_back(B);
+            }
+
+            for (int i = 0; i < A; i++){
+
+                if (i != (A - 1))               // make sure its not the last element
+                    diff = (V[i+1] - V[i]);
+            
+                // make sure we get absolute value
+                if (diff < 0){
+                    diff *= -1;
+                }
+
+                // make sure in bounds
+                if (diff > v.size()){
+                    jolly = false;
+                    break;
+                }
+                // check if we already have that number
+                else if (v[diff-1] > 0){
+                    jolly = false;
+                    break;
+                }
+                else{
+                    v[diff-1]++;
+                }
+            }
+
+            for (int i = 0; i < v.size(); i++){
+                if (v[i] == 1){
+                    hits++;
+                }
+            }
+
+            if (hits == v.size()){
+                cout << "Jolly" << endl;
+            }
+            else{
+                cout << "Not Jolly" << endl;
+            }
+
+            V.clear();
+            v.clear();                           //MIGHT NOT NEED TO CLEAR THIS ONE
+            hits = 0;
+        }
+    }
+    return 0;
+}
